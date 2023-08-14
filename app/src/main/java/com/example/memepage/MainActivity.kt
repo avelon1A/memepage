db25f639249d64d07cdee9521c42bd42d7423837
avelon1A <amanavelon@gmail.com>
2023-08-15 01:25:17 +0530

    ui has been improved

--- app/src/main/java/com/example/memepage/MainActivity.kt
+++ app/src/main/java/com/example/memepage/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.memepage
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
@@ -22,10 +25,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @SuppressLint("SuspiciousIndentation")
     fun memeloader() {
 
 // ...
   val imagee:ImageView = findViewById(R.id.imageview)
+      val   progressBar:ProgressBar = findViewById(R.id.progressBar)
+          progressBar.visibility = View.VISIBLE
 
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.com/gimme"
@@ -33,9 +39,18 @@ class MainActivity : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener { response ->
               val url = response.getString("url")
+
                 Picasso.get()
                     .load(url)
-                    .into(imagee)
+                    .into(imagee, object : Callback {
+                        override fun onSuccess() {
+                            progressBar.visibility = View.GONE
+                        }
+
+                        override fun onError(e: Exception?) {
+                            // Handle error
+                        }
+                    })
             },
             Response.ErrorListener { error ->
                 // TODO: Handle error
@@ -50,5 +65,8 @@ class MainActivity : AppCompatActivity() {
     fun next(view: View) {
         memeloader()
     }
+
+    fun share(view: View) {}
 }
+
 
